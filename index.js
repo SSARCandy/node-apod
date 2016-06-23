a6092ca2d56668d40734d1d1c770522c91c266fd
ssarcandy <ssarcandy@gmail.com>
2016-06-23 16:08:09 -0400

    cros

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 var express = require('express');
-var apod = require('node-apod');
-var app = express();
+var apod    = require('node-apod');
+var app     = express();
 
 var API_KEY = "WmtWE15aVRPb5XBcI7WEimtMyevbhKqNhJun8PgE";
+
+app.all('*', function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.get('/:lang', function (req, res) {
   apod(API_KEY, req.params.lang, function(err, data) {
